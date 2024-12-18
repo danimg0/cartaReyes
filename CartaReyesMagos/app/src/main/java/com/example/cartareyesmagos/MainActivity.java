0e7fd0d826af3448bf8dfafe0f1c99f607c729e2
Nazaret <nazaretatalasianandrades@gmail.com>
2024-12-18 14:10:48 +0100

    UPD: Parte Nazaret

--- CartaReyesMagos/app/src/main/java/com/example/cartareyesmagos/MainActivity.java
+++ CartaReyesMagos/app/src/main/java/com/example/cartareyesmagos/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.cartareyesmagos;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(view,
+                        "Carta de sus majestades los RRMM, enviada con éxito",
+                        Snackbar.LENGTH_LONG);
+                snackbar.setBackgroundTint(Color.parseColor("#37E243"));
+                snackbar.setTextColor(Color.WHITE);
+                snackbar.show();
+
+                Intent resumenCarta = new Intent(MainActivity.this, ResumenCarta.class);
+                startActivity(resumenCarta);
+            }
+        });
     }
 }

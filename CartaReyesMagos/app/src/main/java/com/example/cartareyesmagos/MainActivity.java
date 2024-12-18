48fd0fa30d6c20794c3eff4a7452d3c6c6623a76
Dani <daniel.martos@hotmail.com>
2024-12-18 13:57:58 +0100

    Subo parte Dani

--- CartaReyesMagos/app/src/main/java/com/example/cartareyesmagos/MainActivity.java
+++ CartaReyesMagos/app/src/main/java/com/example/cartareyesmagos/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cartareyesmagos;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nombre;
+    EditText regalo1;
+    EditText regalo2;
+    EditText regalo3;
+    EditText regalo4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        nombre = findViewById(R.id.edNombre);
+        regalo1 = findViewById(R.id.edItem1);
+        regalo2 = findViewById(R.id.edItem2);
+        regalo3 = findViewById(R.id.edItem3);
+        regalo4 = findViewById(R.id.edItem4);
     }
 }
